2354fd6c00c49fa32ac512a0420324f69de27d60
Alex Nichol <alexnichol@comcast.net>
2013-02-24 12:08:35 -0500

    Initial Commit

---
+++ DefinitionSearch/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  DefinitionSearch
+//
+//  Created by Alex Nichol on 2/24/13.
+//  Copyright (c) 2013 Alex Nichol. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ANAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ DefinitionSearch/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DefinitionSearch
+//
+//  Created by Alex Nichol on 2/24/13.
+//  Copyright (c) 2013 Alex Nichol. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
